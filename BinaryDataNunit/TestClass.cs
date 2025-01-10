32494690bbc0216f79126898154115ff3ea39104
m-nash <64171366+m-nash@users.noreply.github.com>
2025-01-10 14:22:24 -0800

    repro of empty bd

---
+++ BinaryDataNunit/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace BinaryDataNunit
+{
+    public class TestClass
+    {
+        [TestCaseSource(typeof(TestSource), nameof(TestSource.EmptyBinaryData))]
+        public void TestMethod1(BinaryData binaryData)
+        {
+        }
+
+        [TestCaseSource(typeof(TestSource), nameof(TestSource.NonEmptyBinaryData))]
+        public void TestMethod2(BinaryData binaryData)
+        {
+        }
+    }
+}
---
+++ BinaryDataNunit/TestSource.cs
@@ -0,0 +1,15 @@
+﻿namespace BinaryDataNunit
+{
+    public static class TestSource
+    {
+        internal static List<BinaryData> EmptyBinaryData =
+        [
+            BinaryData.Empty,
+        ];
+
+        internal static List<BinaryData> NonEmptyBinaryData =
+        [
+            new BinaryData(" "u8.ToArray()),
+        ];
+    }
+}
